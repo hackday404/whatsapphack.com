dcc8163c68a691ee6e179becd36564666d04880d
hackday404 <54924067+hackday404@users.noreply.github.com>
2019-11-01 01:01:46 +0700

    Add files via upload

---
+++ sadapwaEdit.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/bash
+#Tools SadapWhatsApp
+#VIRUS HACKDAY CYBER TEAM
+#By Mr.HACKDAY
+#Jangan DiRecord Tod Gua Bikinya Susah BABI!!!
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+cd /sdcard
+rm -rf aditya
+echo " "
+echo  $blue "<|======================================|>"
+echo  $blue "<|          Tools sadap Whatsapp        |>"
+echo  $blue "<|                                      |>"
+echo  $blue "<| Name: MrHACKDAY    ||    ATTACKER    |>"
+echo  $blue "<| MyTeam: CYBER NET MUSLIM             |>"
+echo  $blue "<| Contact: private                     |>"
+echo  $blue "<| Thanks To: CNM CYBER TEAM            |>"
+echo  $blue "<|======================================|>"
+echo $blue"╭──[Silahkan Masukan Nomernya]>"
+read -p"╰───────root@Hackday=•>" MR.HACKDAY;
+sleep 2
+echo $red "[+]Scaning...."
+sleep 3
+echo $blue "10%"
+sleep 4
+echo $green "20%"
+sleep 2
+echo $cyan "30%"
+sleep 8
+echo $purple "40%"
+sleep 3
+echo $blue "50%"
+sleep 2
+echo $red "60%"
+sleep 5
+echo $green "70%"
+sleep 6
+echo $cyan "80%"
+sleep 7
+echo $purple "90%"
+sleep 3
+echo $blue "100%"
+sleep 2
+echo $green "Prosessing Done"
+sleep 1
+echo  $green "TUNGGU SAMPE SELESAI"
+sleep 1
+echo  $red   "TEAM HACKDAY TERSAKITI"
+sleep 1
+echo  $purple "MEMBUNUH TANPA MENYENTUH"
+sleep 1
+echo  $green "Fuck You !!"
+sleep 1
+echo  $red "i will kill you"
+sleep 1
+echo  $green "REMAMBER WE ARE ANONYMOUS WE DON'T NOT FORGIVE"
+sleep 1
+echo  $green "WE DON'T NOT FORGET EXPECT US!!"
+pkg install cmatrix
+cmatrix
+exit
+fi

